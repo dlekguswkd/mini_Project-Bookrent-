63f7b185aee7fccc1a7e5f0cf03d3fd00c09c976
dlekguswkd <dlekguswkd@naver.com>
2024-07-31 17:51:10 +0900

    bookrent

--- src/bookrent/UserApp.java
+++ src/bookrent/UserApp.java
@@ -1,10 +1,31 @@
 package bookrent;
 
+import java.util.Scanner;
+
 public class UserApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//회원가입		로그인	히스토리		회원수정
+		
+		UserDao userDao = new UserDao();
 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("회원정보를 입력하세요");
+		System.out.print("아이디: ");
+		String id = sc.nextLine();
+		System.out.print("비밀번호: ");
+		String pw = sc.nextLine();
+		System.out.print("이름: ");
+		String name = sc.nextLine();
+		System.out.print("전화번호: ");
+		String pnum = sc.nextLine();
+		System.out.print("이메일: ");
+		String email= sc.nextLine();
+		userDao.insertUser(id, pw, name, pnum, email);
+
+		
+		sc.close();
 	}
 
 }
--- src/bookrent/UserDao.java
+++ src/bookrent/UserDao.java
@@ -1,5 +1,137 @@
 package bookrent;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserDao {
+
+	// 필드
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://192.168.0.73:3306/bookrent_db";
+	private String id = "dahyun";
+	private String pw = "1234";
+
+	// 생성자
+
+	// 메소드 gs
+
+	// 메소드 일반
+
+	private void getConnection() {
+		// DB연결메소드
+		try {
+
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName(driver);
+
+			// 2. Connection 얻어오기
+			conn = DriverManager.getConnection(url, id, pw);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+	}
+
+	private void close() {
+		// 5. 자원정리
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+	}
+
+	// 회원가입
+	public int insertUser(String userId, String password, String userName, String phoneNum, String email) {
+
+		this.getConnection();
+
+		int count = -1;
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			// sql문 준비
+			String query = "";
+			query += " insert into user ";
+			query += " values (null, ?, ?, ?,?,?) ";
+
+			// 바인딩 (말랑말랑하게 해주기)
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, userId);
+			pstmt.setString(2, password);
+			pstmt.setString(3, userName);
+			pstmt.setString(4, phoneNum);
+			pstmt.setString(5, email);
+
+			// 실행
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+		this.close();
+
+		return count;
+
+	}
+
+	// 로그인
+	public int login(String userId, String password) {
+
+		int count = -1;
+
+		this.getConnection();
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			// sql문 준비
+			String query = "";
+			query += "insert into user";
+			query += "values (null, ?, ?, ?, ?,?,?)";
+
+			// 바인딩 (말랑말랑하게 해주기)
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, userId);
+			pstmt.setString(2, password);
+
+			// 실행
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+			System.out.println(count + "건 등록되었습니다.");
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+		this.close();
+
+		return count;
+	}
+
+	// 히스토리
+
+	// 회원수정
 
 }
