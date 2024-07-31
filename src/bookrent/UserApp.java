c67ba560ec57632c13536f21c6e7f2b588beda64
dlekguswkd <dlekguswkd@naver.com>
2024-07-31 22:11:24 +0900

    bookrent

--- src/bookrent/UserApp.java
+++ src/bookrent/UserApp.java
@@ -1,5 +1,6 @@
 package bookrent;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class UserApp {
@@ -14,9 +15,14 @@ public class UserApp {
 		Scanner sc = new Scanner(System.in);
 		boolean run = true;
 		
+		int loginUserNo;
+		
+		int updateUser;
+		
 		System.out.println("도서 대여 프로그램에 온걸 환영합니다!");
 		System.out.println("");
 		System.out.println("원하시는 옵션을 선택해주세요.");
+		
 		
 		while (run) {
 
@@ -31,13 +37,78 @@ public class UserApp {
         
 			case 1: //로그인
 				
+				loginUserNo = 0;
+				
 				System.out.println("[로그인]");
 				System.out.println("회원정보를 입력하세요");
 				System.out.print("아이디: ");
-				
+				String lid = sc.nextLine();
 				System.out.print("비밀번호: ");
-
+				String lpw = sc.nextLine();
         	
+				loginUserNo = userDao.login(lid, lpw);
+				
+				if (loginUserNo <= 0) {
+					System.out.println("회원가입 되지않은 로그인 정보입니다");
+					
+				}else {
+					
+					System.out.println("");
+					System.out.println("원하시는 옵션을 선택해주세요.");
+					
+					while (run) {
+						System.out.println("1.히스토리      2.정보수정       3.종료");
+						System.out.println("--------------------------------");
+						System.out.print(">메뉴번호: ");
+						int menuNo = sc.nextInt();
+						sc.nextLine(); // 숫자 뒤에 문자열 받을때 생기는 오류 수정용
+					
+						switch (menuNo) {
+				        
+						case 1: //히스토리
+							
+							userDao.displayUserRentals(loginUserNo);
+							//System.out.println(userList);
+							
+							//System.out.println("히스토리");
+							break;
+							
+						case 2: //정보수정
+							
+							int result;
+						      
+					         int userNum = loginUserNo;
+					         System.out.print("새 비밀번호: ");
+					         String password = sc.next();
+					         System.out.print("새 전화번호: ");
+					         String phoneNum = sc.next();
+					         System.out.print("새 이메일: ");
+					         String email = sc.next();
+					         
+					         result = userDao.updateUser(userNum, password, phoneNum, email);
+					         if (result > 0) {
+					             System.out.println("회원 정보 수정 성공");
+					         } else {
+					             System.out.println("회원 정보 수정 실패");
+					         }
+					
+							break;
+							
+						case 3:	// 종료
+							
+							run = false;
+							break;
+							
+						default:
+							System.out.println("[다시 입력해 주세요.]");
+							System.out.println("");
+							break;
+							
+						}	// switch() 종료
+					
+					}	// while() 종료
+					
+				}	// if{} 종료 (로그인)
         	
         	
 				break;     
--- src/bookrent/UserDao.java
+++ src/bookrent/UserDao.java
@@ -5,6 +5,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class UserDao {
 
@@ -99,39 +102,91 @@ public class UserDao {
 	// 로그인
 	public int login(String userId, String password) {
 
-		int count = -1;
-
 		this.getConnection();
 
 		try {
 
-			// 3. SQL문 준비 / 바인딩 / 실행
-			// sql문 준비
 			String query = "";
-			query += "insert into user";
-			query += "values (null, ?, ?, ?, ?,?,?)";
+			query += " select user_num, user_name, user_id from user ";
+			query += " where user_id = ?  ";
+			query += " and password = ? ";
 
-			// 바인딩 (말랑말랑하게 해주기)
 			pstmt = conn.prepareStatement(query);
 			pstmt.setString(1, userId);
 			pstmt.setString(2, password);
 
-			// 실행
-			count = pstmt.executeUpdate();
+			ResultSet rs = pstmt.executeQuery();
 
-			// 4.결과처리
-			System.out.println(count + "건 등록되었습니다.");
+			if (rs.next() == true) {
+				// System.out.println("로그인 성공");
+				int userNo = rs.getInt("user_num");
+				return userNo;
+				// System.out.println(userNo);
+			} else {
+				// System.out.println("로그인 실패");
+				return 0;
+			}
 
 		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		}
-		this.close();
+			e.printStackTrace();
+			return -1;
 
-		return count;
+		}
+
 	}
 
 	// 히스토리
+	public void displayUserRentals(int userNum) {
+	      this.getConnection();
+	      try {
+	         String query = "";
+	         query += " SELECT b.title ";
+	         query += "        ,b.author ";
+	         query += "       ,h.rent_date ";
+	         query += "       ,h.return_date ";
+	         query += " FROM history h ";
+	         query += " JOIN book b ON h.book_id = b.book_id ";
+	         query += " WHERE h.user_num = ? ";
+	         query += " ORDER BY h.rent_date DESC ";
 
+	         pstmt = conn.prepareStatement(query);
+	         pstmt.setInt(1, userNum);
+	         rs = pstmt.executeQuery();
+	         System.out.println("===== 대여 및 반납 현황 =====");
+
+	         while (rs.next()) {
+	            String title = rs.getString("title");
+	            String author = rs.getString("author");
+	            String rentDate = rs.getString("rent_date");
+	            String returnDate = rs.getString("return_date");
+	            System.out.println("책 제목: " + title + ", 작가: " + author + ", 대여 날짜: " + rentDate + ", 반납 날짜: "
+	                  + (returnDate != null ? returnDate : "반납 안됨"));
+	         }
+	      } catch (SQLException e) {
+	         System.out.println("error:" + e);
+	      } 
+	         this.close();
+	      }
+
+	// -------------------------------------------------------------------------
 	// 회원수정
+	public int updateUser(int userNum, String password, String phoneNum, String email) {
+	      this.getConnection();
+	      int count = -1;
+	      try {
+	         String query = "UPDATE user SET password = ?, phone_num = ?, email = ? WHERE user_num = ?";
+	         pstmt = conn.prepareStatement(query);
+	         pstmt.setString(1, password);
+	         pstmt.setString(2, phoneNum);
+	         pstmt.setString(3, email);
+	         pstmt.setInt(4, userNum);
+	         count = pstmt.executeUpdate();
+	      } catch (SQLException e) {
+	         System.out.println("error:" + e);
+	      } finally {
+	         this.close();
+	      }
+	      return count;
 
+	}
 }
